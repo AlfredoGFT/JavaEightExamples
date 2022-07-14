a77a0b965193f0c622b0c46643bd427309557dea
Alfredo Marin <alfredo.neto@gft.com>
2022-07-14 09:47:08 -0300

    [DONE] - Optional exercises

--- src/_03ThirdExercises/BookOptional.java
+++ src/_03ThirdExercises/BookOptional.java
@@ -3,10 +3,10 @@ package _03ThirdExercises;
 import model.Author;
 import model.Book;
 import model.Genre;
+import util.BookAutoBuilder;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class BookOptional {
 
@@ -41,5 +41,30 @@ public class BookOptional {
             return Optional.empty();
         }
         return Optional.of(new Book("Optional", new HashSet<>(), 2, new ArrayList<>()));
+    }
+
+    public void optionalPowers(boolean fullValues) {
+        Optional<List<Book>> optionalBooks;
+        if (fullValues) {
+            optionalBooks = Optional.of(BookAutoBuilder.buildMultipleBooks(BookAutoBuilder.THOUSAND, BookAutoBuilder.THOUSAND));
+        } else {
+            optionalBooks = Optional.empty();
+        }
+
+        String names = optionalBooks.map(books -> books.stream().map(Book::getName).collect(Collectors.joining(" - ")))
+                .orElse("Ops! How unfortunate!");
+        System.out.println("Names: " + names);
+
+        Integer totalVolumes = optionalBooks.map(books -> books.stream().map(Book::getVolume).reduce(0, Integer::sum))
+                .orElseGet(() -> returnNotFoundCode());
+        System.out.println("Total volumes: " + totalVolumes);
+
+        String nameVolumes =  optionalBooks.map(books -> books.stream().map(book -> book.getName() + " " + book.getVolume()).collect(Collectors.joining(", ")))
+                .orElseThrow(() -> new RuntimeException("Ops! How unfortunate!"));
+        System.out.println("Name with volumes: " + nameVolumes);
+    }
+
+    private Integer returnNotFoundCode() {
+        return 404;
     }
 }
--- src/_03ThirdExercises/OptionalPredicateExercises.java
+++ src/_03ThirdExercises/OptionalPredicateExercises.java
@@ -7,17 +7,37 @@ public class OptionalPredicateExercises {
         BookOptional bookOptional = new BookOptional();
 
         try {
+            System.out.println("\n--Start first--");
             bookOptional.getBooksException();
             System.out.println("--Finished first--");
         } catch (NullPointerException e) {
-            System.out.println("--YEP! NULL POINTER!--");
+            System.out.println("--Null Pointer! First--");
         }
 
         try {
+            System.out.println("\n--Start second--");
             bookOptional.getBooksOptional();
             System.out.println("--Finished second--");
         } catch (NullPointerException e) {
-            System.out.println("--NULL POINTER...?--");
+            System.out.println("--Null Pointer! Second--");
+        }
+
+        try {
+            System.out.println("\n--Start third--");
+            bookOptional.optionalPowers(true);
+            System.out.println("--Finished third--");
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            System.out.println("--Runtime Exception! Third--");
+        }
+
+        try {
+            System.out.println("\n--Start fourth--");
+            bookOptional.optionalPowers(false);
+            System.out.println("--Finished fourth--");
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            System.out.println("--Runtime Exception! Fourth--");
         }
     }
 }
